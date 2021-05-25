e4c3b23e9fb79b32844684454d1e62f3354d260d
justin <justin@globaloutliers.com>
2021-05-25 13:45:15 +0900

    finished

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-const fastify = require('fastify')({
-  logger: true
-})
+const fastify = require("fastify")({
+  logger: false,
+});
 
 const timeout = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
+fastify.get("/:second", async (request, reply) => {
+  if (request.params.second) {
+    await timeout(Number(request.params.second) * 1000);
+  } else {
+    await timeout(5000);
+  }
 
-fastify.get('/', async (request, reply) => {
-	if (request
-
-	await timeout(3000);
-
-  return { hello: 'world' }
-})
+  return {
+    hello: "world",
+    elapsedTime: `${request.params.second} seconds` || 5 + " seconds",
+  };
+});
 
 const start = async () => {
   try {
-    await fastify.listen(3015)
+    await fastify.listen(3015, () => console.log("Server started at 3015 🚀 "));
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-}
-start()
+};
+start();
